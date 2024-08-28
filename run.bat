d3179aa95c5cc51857b9dcefef89d0f3e60a1792
Chalwk77 <jericho.crosby227@gmail.com>
2024-08-29 10:52:12 +1200

    Refactor

--- run.bat
+++ run.bat
@@ -1,5 +1,27 @@
-ECHO off
+@echo off
 
+:: Set current directory to the batch file's directory
 cd "%~dp0"
 
-start java -jar bot.jar
+:: Create log file
+echo ===========================>> run.log
+echo Starting bot...>> run.log
+date /t >> run.log
+time /t >> run.log
+echo ===========================>> run.log
+
+:: Run Java JAR file
+start "PGR2" java -jar bot.jar 2>> run.log
+
+:: Check for errors
+if %ERRORLEVEL% NEQ 0 (
+    echo ===========================>> run.log
+    echo Error: Failed to start bot!>> run.log
+    echo ===========================>> run.log
+    pause
+    exit /b 1
+)
+
+echo ===========================>> run.log
+echo Bot started successfully!>> run.log
+echo ===========================>> run.log
--- run.sh
+++ run.sh
@@ -1,1 +1,21 @@
-java -jar bot.jar
+#!/bin/bash
+# Create log file
+echo "==========================" >> run.log
+echo "Starting bot..." >> run.log
+date "+%Y-%m-%d %T" >> run.log
+echo "==========================" >> run.log
+
+# Run Java JAR file
+java -jar PGR2.jar 2>> run.log
+
+# Check for errors
+if [ $? -ne 0 ]; then
+echo "==========================" >> run.log
+    echo "Error: Failed to start bot!" >> run.log
+    echo "==========================" >> run.log
+    exit 1
+fi
+# Write success message to log file
+echo "==========================" >> run.log
+echo "Bot started successfully!" >> run.log
+echo "==========================" >> run.log
